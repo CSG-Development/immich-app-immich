5f94fd3199a48ac02c31fddcfd777911f8517c5a
Roman Pupin <roman.pupin@noveogroup.com>
2025-06-20 12:47:08 +0300

    chore: change bundleId ios

--- mobile/ios/Runner/BackgroundSync/BackgroundSyncWorker.swift
+++ mobile/ios/Runner/BackgroundSync/BackgroundSyncWorker.swift
@@ -25,7 +25,7 @@ class BackgroundSyncWorker {
         name: "BackgroundImmich"
     )
     
-    let notificationId = "com.seagate.curator.stxphotos.app/backgroundNotifications"
+    let notificationId = "com.seagate.curator.stxphotos/backgroundNotifications"
     // The background message passing channel
     var channel: FlutterMethodChannel?
     
