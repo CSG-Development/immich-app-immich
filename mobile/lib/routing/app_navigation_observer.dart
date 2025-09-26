5440e7e89f1aff563053300124dba20831cadb2b
Roman Pupin <roman.pupin@noveogroup.com>
2025-09-26 10:26:29 +0300

    feat: android app update

--- mobile/lib/routing/app_navigation_observer.dart
+++ mobile/lib/routing/app_navigation_observer.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/providers/routes.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
+import 'package:immich_mobile/providers/infrastructure/app_update.provider.dart';
+import 'package:flutter/widgets.dart' show WidgetsBinding;
 
 class AppNavigationObserver extends AutoRouterObserver {
   /// Riverpod Instance
   final WidgetRef ref;
+  bool _updateCheckDone = false;
 
   AppNavigationObserver({required this.ref});
 
@@ -24,6 +27,17 @@ class AppNavigationObserver extends AutoRouterObserver {
       ref.read(previousRouteNameProvider.notifier).state = previousRoute?.settings.name;
       ref.read(previousRouteDataProvider.notifier).state = previousRoute?.settings;
     });
+
+    // Run app update check once after leaving splash (cold boot)
+    final currentName = route.settings.name;
+    if (!_updateCheckDone && currentName != SplashScreenRoute.name) {
+      _updateCheckDone = true;
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        final ctx = ref.read(appRouterProvider).navigatorKey.currentContext;
+        if (ctx == null) return;
+        ref.read(appUpdateServiceProvider).checkOnStart(context: ctx);
+      });
+    }
   }
 
   _handleLockedViewState(Route route, Route? previousRoute) {
