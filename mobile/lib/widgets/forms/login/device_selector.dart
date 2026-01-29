3a06d78ea60c384b6d151f86c13362fbd1862e60
Roman Pupin <roman.pupin@noveogroup.com>
2026-01-29 11:01:29 +0300

    feat: Update the icon of the device in Connecting to field

--- mobile/lib/widgets/forms/login/device_selector.dart
+++ mobile/lib/widgets/forms/login/device_selector.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hc_device/hc_device.dart' show DeviceItem;
 import 'package:immich_mobile/widgets/forms/login/server_endpoint_input.dart';
 
@@ -74,11 +75,10 @@ class DeviceSelector extends HookWidget {
         if (isDetecting) {
           icon = const Icon(Icons.search, size: 32);
         } else {
-          // icon = const Icon(Icons.cloud_off, size: 32, color: Colors.red);
           icon = null;
         }
       } else {
-        icon = Image.asset("assets/device.webp", width: 50, height: 40);
+        icon = UnconstrainedBox(child: SvgPicture.asset('assets/device.svg', width: 24.0, height: 24.0));
       }
       return icon != null
           ? Padding(
