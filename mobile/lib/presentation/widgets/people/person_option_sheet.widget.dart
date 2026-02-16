c96a86746ae8c10f22b6b1c7ba956b8fe4b2da12
Roman Pupin <roman.pupin@noveogroup.com>
2026-02-16 10:32:18 +0300

    feat: Merge faces found that are the same person

--- mobile/lib/presentation/widgets/people/person_option_sheet.widget.dart
+++ mobile/lib/presentation/widgets/people/person_option_sheet.widget.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/extensions/translate_extensions.dart';
 
 class PersonOptionSheet extends ConsumerWidget {
-  const PersonOptionSheet({super.key, this.onEditName, this.onEditBirthday, this.birthdayExists = false});
+  const PersonOptionSheet({super.key, this.onEditName, this.onEditBirthday, this.onMerge, this.birthdayExists = false});
 
   final VoidCallback? onEditName;
   final VoidCallback? onEditBirthday;
+  final VoidCallback? onMerge;
   final bool birthdayExists;
 
   @override
@@ -28,6 +31,20 @@ class PersonOptionSheet extends ConsumerWidget {
               leading: const Icon(Icons.cake),
               title: Text((birthdayExists ? 'edit_birthday' : "add_birthday").t(context: context), style: textStyle),
               onTap: onEditBirthday,
+            ),
+            ListTile(
+              leading: SizedBox(
+                width: 24.0,
+                height: 24.0,
+                child: Center(
+                  child: SvgPicture.asset(
+                    'assets/merge-people-menu-item.svg',
+                    colorFilter: ColorFilter.mode(context.colorScheme.onSurface, BlendMode.srcIn),
+                  ),
+                ),
+              ),
+              title: Text(("merge_people").t(context: context), style: textStyle),
+              onTap: onMerge,
             ),
           ],
         ),
