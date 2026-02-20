84ebfb2e9157b24b6232462ed6cc8bfdea8ee731
Dmitrii Lysenko <dmitrii.lysenko@noveogroup.com>
2026-02-20 14:45:32 +0500

    replace-face-recognition-ai-model fix type error

--- server/src/repositories/search.repository.ts
+++ server/src/repositories/search.repository.ts
@@ -81,7 +81,7 @@ export interface SearchExifOptions {
 
 export interface SearchEmbeddingOptions {
   embedding: string;
-  counterEmbedding: string;
+  counterEmbedding?: string;
   userIds: string[];
 }
 
