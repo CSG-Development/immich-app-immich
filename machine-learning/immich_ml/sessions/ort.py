03d6c7223de2e9b95080ffd5ba079a7cf9e959aa
Andrey Chernyavskiy <achernyavskiy@noveogroup.com>
2026-05-30 02:13:11 +0500

    ort.py settings has been updated

--- machine-learning/immich_ml/sessions/ort.py
+++ machine-learning/immich_ml/sessions/ort.py
@@ -93,8 +93,8 @@ class OrtSession:
                 case "OpenVINOExecutionProvider":
                     options = {
                         "device_type": f"GPU.{settings.device_id}",
-                        "precision": "FP16",
-                        # "precision": "FP32",
+                        #"precision": "FP16",
+                         "precision": "FP32",
                         # "cache_dir": (self.model_path.parent / "openvino").as_posix(),
                     }
                 case _:
